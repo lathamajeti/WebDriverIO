fb434a19eb4b0b7deea9e76ba96a976269ee02d3
Suma <sumalathamajeti@gmail.com>
2023-09-20 17:14:30 -0700

    my commit for webdriverio test

---
+++ test/specs/test.mydemo.js
@@ -0,0 +1,9 @@
+describe('My Demo application', () => {
+
+    it('login with valid credentials', async () => {
+        browser.url('https://the-internet.herokuapp.com/login')
+        await $('#username').setValue("tomsmith")
+        await $('#password').setValue("SuperSecretPassword!")
+        await $('button[type="submit"]').click()
+    })
+})
